44069975e8c5ccedd637325df1db8b1408ed7995
mhutch14 <hutchimw@miamioh.edu>
2017-05-09 23:32:05 -0400

    Add files via upload

---
+++ TembooAccount.h
@@ -0,0 +1,25 @@
+/*
+IMPORTANT NOTE about TembooAccount.h
+
+TembooAccount.h contains your Temboo account information and must be included
+alongside your sketch. To do so, make a new tab in Arduino, call it TembooAccount.h,
+and copy this content into it. 
+*/
+
+#define TEMBOO_ACCOUNT "ece-387-final-project"  // Your Temboo account name 
+#define TEMBOO_APP_KEY_NAME "myFirstApp"  // Your Temboo app key name
+#define TEMBOO_APP_KEY "xghNLYZn543sEj7vV8v82Q66rRMVs7rU"  // Your Temboo app key
+#define TEMBOO_DEVICE_TYPE "mega+w101"
+
+#define WIFI_SSID "JonesWIFI"
+#define WPA_PASSWORD "livijosh2012"
+
+#if TEMBOO_LIBRARY_VERSION < 2
+#error "Your Temboo library is not up to date. You can update it using the Arduino library manager under Sketch > Include Library > Manage Libraries..."
+#endif
+
+/* 
+The same TembooAccount.h file settings can be used for all Temboo SDK sketches.  
+Keeping your account information in a separate file means you can share the 
+main .ino file without worrying that you forgot to delete your credentials.
+*/
---
+++ Texting_Test.ino
@@ -0,0 +1,92 @@
+#include <SPI.h>
+#include <WiFi101.h>
+#include <WiFiSSLClient.h>
+#include <TembooSSL.h>
+#include "TembooAccount.h" // Contains Temboo account information
+
+WiFiSSLClient client;
+
+int calls = 1;   // Execution count, so this doesn't run forever
+int maxCalls = 10;   // Maximum number of times the Choreo should be executed
+
+void setup() {
+  WiFi.setPins(8, 7, 4);
+  
+  Serial.begin(9600);
+  
+  // For debugging, wait until the serial console is connected
+  delay(4000);
+  while(!Serial);
+
+  int wifiStatus = WL_IDLE_STATUS;
+
+  // Determine if the WiFi Shield is present
+  Serial.print("\n\nShield:");
+  if (WiFi.status() == WL_NO_SHIELD) {
+    Serial.println("FAIL");
+
+    // If there's no WiFi shield, stop here
+    while(true);
+  }
+
+  Serial.println("OK");
+
+  // Try to connect to the local WiFi network
+  while(wifiStatus != WL_CONNECTED) {
+    Serial.print("WiFi:");
+    wifiStatus = WiFi.begin(WIFI_SSID, WPA_PASSWORD);
+
+    if (wifiStatus == WL_CONNECTED) {
+      Serial.println("OK");
+    } else {
+      Serial.println("FAIL");
+    }
+    delay(5000);
+  }
+
+  Serial.println("Setup complete.\n");
+}
+
+void loop() {
+  if (calls <= maxCalls) {
+    Serial.println("Running SendSMS - Run #" + String(calls++));
+
+    TembooChoreoSSL SendSMSChoreo(client);
+
+    // Invoke the Temboo client
+    SendSMSChoreo.begin();
+
+    // Set Temboo account credentials
+    SendSMSChoreo.setAccountName(TEMBOO_ACCOUNT);
+    SendSMSChoreo.setAppKeyName(TEMBOO_APP_KEY_NAME);
+    SendSMSChoreo.setAppKey(TEMBOO_APP_KEY);
+    SendSMSChoreo.setDeviceType(TEMBOO_DEVICE_TYPE);
+
+    // Set Choreo inputs
+    String AuthTokenValue = "12278bab0c5010e064c5165dec0f9ca7";
+    SendSMSChoreo.addInput("AuthToken", AuthTokenValue);
+    String ToValue = "+15138463823";
+    SendSMSChoreo.addInput("To", ToValue);
+    String FromValue = "+18597570564";
+    SendSMSChoreo.addInput("From", FromValue);
+    String BodyValue = "Your Amazon Dash Button product is running low. If you want to confirm purchase open Light Blue App and send buy now command.";
+    SendSMSChoreo.addInput("Body", BodyValue);
+    String AccountSIDValue = "AC83b58bcbfaabba338106e1bc87a0d8ae";
+    SendSMSChoreo.addInput("AccountSID", AccountSIDValue);
+
+    // Identify the Choreo to run
+    SendSMSChoreo.setChoreo("/Library/Twilio/SMSMessages/SendSMS");
+
+    // Run the Choreo; when results are available, print them to serial
+    SendSMSChoreo.run();
+
+    while(SendSMSChoreo.available()) {
+      char c = SendSMSChoreo.read();
+      Serial.print(c);
+    }
+    SendSMSChoreo.close();
+  }
+
+  Serial.println("\nWaiting...\n");
+  delay(30000); // wait 30 seconds between SendSMS calls
+}
